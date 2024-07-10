ceb92bccc98a449f66dff1abf621ea08b1e29855
nixus619 <nickdcedor@gmail.com>
2024-07-10 16:04:40 -0400

    Edited some css and did some of the javascript
    Changed the css to make a more calulator border. Also added better effects surrounding numbers/operands/special buttons. Numbers, special buttons (clear, +/-, %, =) will now have a pushing kind of animation while the operands buttons will stay active.
    Worked on some of the JS to get +,-,/,* all working properly. Need to fix what happens when the user selects a leading 0 and the decimal button doesn't do anything yet. The response section will also allow numbers to go longer than the width. Will have to fix that

--- calc.js
+++ calc.js
@@ -1,1 +1,63 @@
-console.log("Hello World");
+function operate(first, second, operand){
+    if(operand=="+"){
+        return first + second;
+    }
+    if(operand=="-"){
+        return first - second;
+    }
+    if(operand=="*"){
+        return first * second;
+    }
+    if(operand=="/"){
+        return first / second;
+    }
+}
+
+const buttons = document.querySelectorAll("button");
+const answer = document.querySelector(".answer");
+let first = null;
+let second = null;
+let operand = null;
+
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        if(button.classList.contains("number")) {
+            answer.textContent = button.textContent;
+            if(first==null && operand==null){
+                first = parseInt(button.textContent);
+                console.log(first);
+            } else if(first!=null && operand!=null) {
+                if(second==null) {
+                    second = parseInt(button.textContent);
+                } else {
+                    second = second + "" + parseInt(button.textContent);
+                }
+                answer.textContent=second;
+                console.log(second);
+            } else {
+                first = first + "" + parseInt(button.textContent);
+                answer.textContent = first;
+                console.log(first);
+            }
+        }
+        if(button.classList.contains("operand")) {
+            operand = button.textContent;
+            console.log(operand);
+        }
+        if(button.textContent == "="){
+            console.log("Equals clicked");
+            console.log(first);
+            console.log(second);
+            console.log(operand);
+            first = operate(parseInt(first), parseInt(second), operand);
+            operand = null;
+            answer.textContent=first;
+        }
+        if(button.textContent == "C"){
+            first = null;
+            second = null;
+            operand = null;
+            answer.textContent = "0";
+        }
+    });
+});
