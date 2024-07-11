32706f3a5f69553c62178dd27322e03fbed369f4
nixus619 <nickdcedor@gmail.com>
2024-07-11 15:53:12 -0400

    Added special functionalities. Project 90% complete
    +/-, %, and . all work and I think with that all the core functionality is done. The only outstanding issue/bug I can find right now is the overflow on the text box.
    If you have a very large number or a repeating decimal/decimal with a lot of decimals, the text box won't look right. Need to research how to make sure it stays 8 digits and when it doesn't, truncate it.

--- calc.js
+++ calc.js
@@ -1,6 +1,6 @@
 function operate(first, second, operand){
     if(operand=="+"){
-        return first + second;
+        return (first + second);
     }
     if(operand=="-"){
         return first - second;
@@ -9,7 +9,29 @@ function operate(first, second, operand){
         return first * second;
     }
     if(operand=="/"){
-        return first / second;
+        if(second==0){
+            return "Nice try";
+        } else {
+            return first/second;
+        }
+    }
+}
+
+function specialFunction(first,func){
+    if(func=="+/-"){
+        return first * -1;
+    }
+    if(func=="%"){
+        return first/100;
+    }
+    if(func=="."){
+        if(first == NaN) {
+            return "0.";
+        } else if(first%1!=0){
+            return parseFloat(first);
+        } else {
+            return first + ".";
+        }
     }
 }
 
@@ -18,29 +40,65 @@ const answer = document.querySelector(".answer");
 let first = null;
 let second = null;
 let operand = null;
+let complete = false;
 
 buttons.forEach((button) => {
     button.addEventListener("click", () => {
         if(button.classList.contains("number")) {
             answer.textContent = button.textContent;
-            if(first==null && operand==null){
+            if(first==null && operand==null && complete==false){
                 first = parseInt(button.textContent);
                 console.log(first);
             } else if(first!=null && operand!=null) {
                 if(second==null) {
                     second = parseInt(button.textContent);
                 } else {
-                    second = second + "" + parseInt(button.textContent);
+                    if((second==0)) {
+                        if(second.includes(".")){
+                            second = second + "" + parseInt(button.textContent);
+                            answer.textContent = second;
+                        } else {
+                            second = parseInt(button.textContent);
+                            answer.textContent = second;
+                        }
+                    } else{
+                        second = second + "" + parseInt(button.textContent);
+                        answer.textContent = second;
+                    }
                 }
                 answer.textContent=second;
                 console.log(second);
             } else {
-                first = first + "" + parseInt(button.textContent);
-                answer.textContent = first;
+                if(complete == true) {
+                    first = parseInt(button.textContent);
+                    answer.textContent=first;
+                    complete=false;
+                }
+                else if((first==0) && complete==true) {
+                    if(first.includes(".")){
+                        first = first + "" + parseInt(button.textContent);
+                        answer.textContent = first;
+                    } else {
+                        first = parseInt(button.textContent);
+                        answer.textContent = first;
+                        complete=false;
+                    }
+                    
+                } else{
+                    first = first + "" + parseInt(button.textContent);
+                    answer.textContent = first;
+                }
+                
                 console.log(first);
             }
         }
         if(button.classList.contains("operand")) {
+            if(first!=null && operand !=null && second!=null){
+                first = operate(parseFloat(first),parseFloat(second),operand);
+                answer.textContent=first;
+                second=null;
+                complete=true;
+            }
             operand = button.textContent;
             console.log(operand);
         }
@@ -49,15 +107,38 @@ buttons.forEach((button) => {
             console.log(first);
             console.log(second);
             console.log(operand);
-            first = operate(parseInt(first), parseInt(second), operand);
+            if(second!=null){
+                first = operate(parseFloat(first), parseFloat(second), operand);
+            }
             operand = null;
             answer.textContent=first;
+            second=null;
+            complete = true;
         }
         if(button.textContent == "C"){
             first = null;
             second = null;
             operand = null;
             answer.textContent = "0";
+            complete=false;
+        }
+        if(button.textContent == "+/-" || button.textContent=="%" || button.textContent=="."){
+            if(second!=null){
+                second=specialFunction(parseFloat(second),button.textContent);
+                answer.textContent=second;
+            } else {
+                if(first==null){
+                    first=specialFunction(parseFloat(0),button.textContent);
+
+                } else if(operand!=null){
+                    second=specialFunction(parseFloat(0),button.textContent);
+                    answer.textContent=second;
+                }   
+                else {
+                    first=specialFunction(parseFloat(first),button.textContent);
+                    answer.textContent=first
+                }
+            }
         }
     });
 });
